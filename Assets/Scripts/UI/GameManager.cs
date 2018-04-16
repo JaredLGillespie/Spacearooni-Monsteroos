82db1abfbc9e3a827a0ab74f76468ce9f67ad63e
tommyguntragedy <ericatomes@gmail.com>
2018-04-16 18:25:04 -0400

    created pause menu
    can be brought up by hitting 'esc'

---
+++ Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    private GameObject inGameMenu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleEsc();
+	}
+
+    public void HandleEsc()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("test");
+            ShowInGameMenu();
+        }
+    }
+
+    public void ShowInGameMenu() {
+        inGameMenu.SetActive(!inGameMenu.activeSelf);
+    }
+}
