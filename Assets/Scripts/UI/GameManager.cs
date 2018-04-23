58c4cb65e49e7abb027cce0e69bec029198abc37
tommyguntragedy <ericatomes@gmail.com>
2018-04-23 13:57:00 -0400

    Revert "added enemy health bar"
    This reverts commit 439056f9b12fb8d58d29b8074964cb704e978b75.

--- Assets/Scripts/UI/GameManager.cs
+++ Assets/Scripts/UI/GameManager.cs
@@ -2,33 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
     [SerializeField]
-    public GameObject inGameMenu;
-    [SerializeField]
-    public GameObject optionsMenu;
-    [SerializeField]
-    private Text ammoLabel;
-    public int bulletCount=0;
-    public float laserTime = 0.0f;
-    [SerializeField]
-    private Text laserTimer;
-    [SerializeField] public GameObject pistol;
-    [SerializeField] public GameObject laser;
-    [SerializeField] public GameObject machine;
-    [SerializeField] public GameObject rocket;
+    private GameObject inGameMenu;
     // Use this for initialization
     void Start() {
-        ammoLabel.text = "";
-        laserTimer.text = "";
+
     }
 
     // Update is called once per frame
     void Update() {
         HandleEsc();
-        ammoLabel.text = "ammo: " + bulletCount;
-        laserTimer.text = "TIMER: " + laserTime;
     }
 
     public void HandleEsc()
@@ -40,33 +24,14 @@ public class GameManager : MonoBehaviour {
     }
 
     public void ShowInGameMenu() {
-        if (optionsMenu.activeSelf)
+        inGameMenu.SetActive(!inGameMenu.activeSelf);
+        if (!inGameMenu.activeSelf)
         {
-            ShowMain();
+            Time.timeScale = 1;
         }
         else {
-            inGameMenu.SetActive(!inGameMenu.activeSelf);
-            if (!inGameMenu.activeSelf)
-            {
-                Time.timeScale = 1;
-
-            }
-            else
-            {
-                Time.timeScale = 0;
-            }
+            Time.timeScale = 0;
         }
-    }
-
-    public void ShowOptions()
-    {
-        inGameMenu.SetActive(false);
-        optionsMenu.SetActive(true);
-    }
-    public void ShowMain()
-    {
-        inGameMenu.SetActive(true);
-        optionsMenu.SetActive(false);
     }
 
     public void RestartGame() {
