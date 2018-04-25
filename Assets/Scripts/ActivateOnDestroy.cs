21e54c7f7de2330fb8f46866298d59245fd695ae
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-04-24 20:11:29 -0400

    Adding item spawners

---
+++ Assets/Scripts/ActivateOnDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActivateOnDestroy : MonoBehaviour
+{
+    public UnityEvent OnActivation;
+
+    private void OnDestroy()
+    {
+        OnActivation.Invoke();
+    }
+}
