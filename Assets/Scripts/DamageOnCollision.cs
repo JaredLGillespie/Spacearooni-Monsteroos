9dca72c3a358ecf4dbe7632ddb8ca949e8ad209b
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-04-25 02:32:27 -0400

    disks do damage

---
+++ Assets/Scripts/DamageOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class DamageOnCollision : MonoBehaviour
+{
+    [SerializeField] private string DamageTag = "Enemy"; // Object to damage tag
+    [SerializeField] private float Damage; // Amount of damage to do if hit enemy
+
+    private new Collider2D collider2D;
+
+    private void Awake()
+    {
+        collider2D = GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
--- Assets/Scripts/ProximityDamager.cs
+++ Assets/Scripts/ProximityDamager.cs
@@ -9,29 +9,65 @@ public class ProximityDamager : MonoBehaviour
     [SerializeField] private float DamageRadius = 0.15f; // Radius of damage
     [SerializeField] private bool ScaleDamage = true; // If true, damage is 0 at edge and maximum at center
     [SerializeField] private bool DestroyOnStart = true; // Destroy immediately after creation
+    [SerializeField] private bool Continuous = false; // Keep damaging if true
+    [SerializeField] private float DamageInterval = 0.1f;
 
     private void Start()
     {
-        var objects = GameObject.FindGameObjectsWithTag(DamageTag);
-
-        foreach (var o in objects)
+        if (Continuous)
         {
-            var dist = Vector2.Distance(this.transform.position, o.transform.position);
-
-            if (dist <= DamageRadius)
-            {
-                var damage = Damage;
-
-                if (ScaleDamage)
-                    damage = Damage / (DamageRadius - dist);
-
-                var comp = o.GetComponent<DamageObject>();
-                if (comp != null)
-                    comp.InflictDamage(Damage);
-            }
+            StartCoroutine("ContinueDamage");
         }
+        else
+        {
+            var objects = GameObject.FindGameObjectsWithTag(DamageTag);
 
-        if (DestroyOnStart)
-            Destroy(this.gameObject);
+            foreach (var o in objects)
+            {
+                var dist = Vector2.Distance(this.transform.position, o.transform.position);
+
+                if (dist <= DamageRadius)
+                {
+                    var damage = Damage;
+
+                    if (ScaleDamage)
+                        damage = Damage / (DamageRadius - dist);
+
+                    var comp = o.GetComponent<DamageObject>();
+                    if (comp != null)
+                        comp.InflictDamage(Damage);
+                }
+            }
+
+            if (DestroyOnStart)
+                Destroy(this.gameObject);
+        }
+    }
+
+    private IEnumerator ContinueDamage()
+    {
+        while (true)
+        {
+            var objects = GameObject.FindGameObjectsWithTag(DamageTag);
+
+            foreach (var o in objects)
+            {
+                var dist = Vector2.Distance(this.transform.position, o.transform.position);
+
+                if (dist <= DamageRadius)
+                {
+                    var damage = Damage;
+
+                    if (ScaleDamage)
+                        damage = Damage / (DamageRadius - dist);
+
+                    var comp = o.GetComponent<DamageObject>();
+                    if (comp != null)
+                        comp.InflictDamage(Damage);
+                }
+            }
+
+            yield return new WaitForSeconds(DamageInterval);
+        }
     }
 }
