a8dd2f811ea05d281de10e2fe84a3319d717006f
nearly everyone <nearlyeveryone@tutturu.moe>
2018-04-24 20:57:37 -0400

    added astar movement to AI

--- Assets/Scripts/Enemy/AlienMovement.cs
+++ Assets/Scripts/Enemy/AlienMovement.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class AlienMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject player;
+    public GameObject target;
+
+    // Use this for initialization
+    void Start () {
+        Random.InitState(5318008);
+        Seeker seeker = GetComponent<Seeker>();
+        seeker.StartPath(transform.position, target.transform.position);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector3 targetPosition = player.transform.position;
+        float offsetX = Random.Range(-2.0f, -2.0f);
+        float offsetY = Random.Range(-2.0f, -2.0f);
+        targetPosition = player.transform.position;
+        targetPosition.x += offsetX;
+        targetPosition.y += offsetY;
+
+        target.transform.position = targetPosition;
+    }
 }
